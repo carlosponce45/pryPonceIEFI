785a36c3ab0b23abcfe2e95e45eacb70c3bc5ea0
root <mgiraudo@ies21.edu.ar>
2023-06-21 13:52:08 -0300

    Agregar archivos de proyecto.

---
+++ pryPonceIEFI/Club.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace pryPonceIEFI
+{
+    internal class Club
+    {
+        public string nombre { get; set; }
+        public string äpellido { get; set; }
+        public string edad { get; set; }
+        public string lugarnacimiento { get; set; }
+        public string tipodesexo { get; set; }
+        public string ingreso { get; set; }
+        public string puntaje { get; set; }
+
+        object oledbconnection = new OleDbConnection();
+        object oledbcomando = new OleDbCommand();
+        OleDbDataAdapter ojda = new OleDbDataAdapter();
+        
+
+    }
+}
---
+++ pryPonceIEFI/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace pryPonceIEFI
+{
+    partial class pryPonceIEFI
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.lbllugardenacimiento = new System.Windows.Forms.Label();
+            this.lblapellido = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblingreso = new System.Windows.Forms.Label();
+            this.combingreso = new System.Windows.Forms.ComboBox();
+            this.lblpuntaje = new System.Windows.Forms.Label();
+            this.combporcentaje = new System.Windows.Forms.ComboBox();
+            this.btnregistrar = new System.Windows.Forms.Button();
+            this.btnvolver = new System.Windows.Forms.Button();
+            this.comblugardenacimiento = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbnmasculino = new System.Windows.Forms.RadioButton();
+            this.rbnfemenino = new System.Windows.Forms.RadioButton();
+            this.lbledad = new System.Windows.Forms.Label();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Location = new System.Drawing.Point(18, 82);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(60, 13);
+            this.lblnombre.TabIndex = 0;
+            this.lblnombre.Text = "NOMBRE :";
+            // 
+            // lbllugardenacimiento
+            // 
+            this.lbllugardenacimiento.AutoSize = true;
+            this.lbllugardenacimiento.Location = new System.Drawing.Point(12, 185);
+            this.lbllugardenacimiento.Name = "lbllugardenacimiento";
+            this.lbllugardenacimiento.Size = new System.Drawing.Size(135, 13);
+            this.lbllugardenacimiento.TabIndex = 1;
+            this.lbllugardenacimiento.Text = "LUGAR DE NACIMIENTO:";
+            // 
+            // lblapellido
+            // 
+            this.lblapellido.AutoSize = true;
+            this.lblapellido.Location = new System.Drawing.Point(12, 114);
+            this.lblapellido.Name = "lblapellido";
+            this.lblapellido.Size = new System.Drawing.Size(62, 13);
+            this.lblapellido.TabIndex = 2;
+            this.lblapellido.Text = "APELLIDO:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "TIPO DE SEXO:";
+            // 
+            // lblingreso
+            // 
+            this.lblingreso.AutoSize = true;
+            this.lblingreso.Location = new System.Drawing.Point(19, 274);
+            this.lblingreso.Name = "lblingreso";
+            this.lblingreso.Size = new System.Drawing.Size(59, 13);
+            this.lblingreso.TabIndex = 9;
+            this.lblingreso.Text = "INGRESO:";
+            // 
+            // combingreso
+            // 
+            this.combingreso.FormattingEnabled = true;
+            this.combingreso.Location = new System.Drawing.Point(168, 266);
+            this.combingreso.Name = "combingreso";
+            this.combingreso.Size = new System.Drawing.Size(121, 21);
+            this.combingreso.TabIndex = 10;
+            // 
+            // lblpuntaje
+            // 
+            this.lblpuntaje.AutoSize = true;
+            this.lblpuntaje.Location = new System.Drawing.Point(12, 334);
+            this.lblpuntaje.Name = "lblpuntaje";
+            this.lblpuntaje.Size = new System.Drawing.Size(59, 13);
+            this.lblpuntaje.TabIndex = 11;
+            this.lblpuntaje.Text = "PUNTAJE:";
+            // 
+            // combporcentaje
+            // 
+            this.combporcentaje.FormattingEnabled = true;
+            this.combporcentaje.Location = new System.Drawing.Point(161, 326);
+            this.combporcentaje.Name = "combporcentaje";
+            this.combporcentaje.Size = new System.Drawing.Size(121, 21);
+            this.combporcentaje.TabIndex = 12;
+            // 
+            // btnregistrar
+            // 
+            this.btnregistrar.Location = new System.Drawing.Point(22, 385);
+            this.btnregistrar.Name = "btnregistrar";
+            this.btnregistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnregistrar.TabIndex = 13;
+            this.btnregistrar.Text = "REGISTRAR";
+            this.btnregistrar.UseVisualStyleBackColor = true;
+            // 
+            // btnvolver
+            // 
+            this.btnvolver.Location = new System.Drawing.Point(207, 385);
+            this.btnvolver.Name = "btnvolver";
+            this.btnvolver.Size = new System.Drawing.Size(75, 23);
+            this.btnvolver.TabIndex = 14;
+            this.btnvolver.Text = "VOLVER";
+            this.btnvolver.UseVisualStyleBackColor = true;
+            // 
+            // comblugardenacimiento
+            // 
+            this.comblugardenacimiento.FormattingEnabled = true;
+            this.comblugardenacimiento.Location = new System.Drawing.Point(188, 177);
+            this.comblugardenacimiento.Name = "comblugardenacimiento";
+            this.comblugardenacimiento.Size = new System.Drawing.Size(121, 21);
+            this.comblugardenacimiento.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(25, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(353, 44);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "CLUB LOS SAUCE";
+            // 
+            // rbnmasculino
+            // 
+            this.rbnmasculino.AutoSize = true;
+            this.rbnmasculino.Location = new System.Drawing.Point(132, 223);
+            this.rbnmasculino.Name = "rbnmasculino";
+            this.rbnmasculino.Size = new System.Drawing.Size(88, 17);
+            this.rbnmasculino.TabIndex = 17;
+            this.rbnmasculino.TabStop = true;
+            this.rbnmasculino.Text = "MASCULINO";
+            this.rbnmasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbnfemenino
+            // 
+            this.rbnfemenino.AutoSize = true;
+            this.rbnfemenino.Location = new System.Drawing.Point(243, 223);
+            this.rbnfemenino.Name = "rbnfemenino";
+            this.rbnfemenino.Size = new System.Drawing.Size(81, 17);
+            this.rbnfemenino.TabIndex = 18;
+            this.rbnfemenino.TabStop = true;
+            this.rbnfemenino.Text = "FEMENINO";
+            this.rbnfemenino.UseVisualStyleBackColor = true;
+            // 
+            // lbledad
+            // 
+            this.lbledad.AutoSize = true;
+            this.lbledad.Location = new System.Drawing.Point(12, 149);
+            this.lbledad.Name = "lbledad";
+            this.lbledad.Size = new System.Drawing.Size(40, 13);
+            this.lbledad.TabIndex = 19;
+            this.lbledad.Text = "EDAD:";
+            // 
+            // txtedad
+            // 
+            this.txtedad.Location = new System.Drawing.Point(132, 142);
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(37, 20);
+            this.txtedad.TabIndex = 20;
+            // 
+            // pryPonceIEFI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 434);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.lbledad);
+            this.Controls.Add(this.rbnfemenino);
+            this.Controls.Add(this.rbnmasculino);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comblugardenacimiento);
+            this.Controls.Add(this.btnvolver);
+            this.Controls.Add(this.btnregistrar);
+            this.Controls.Add(this.combporcentaje);
+            this.Controls.Add(this.lblpuntaje);
+            this.Controls.Add(this.combingreso);
+            this.Controls.Add(this.lblingreso);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblapellido);
+            this.Controls.Add(this.lbllugardenacimiento);
+            this.Controls.Add(this.lblnombre);
+            this.Name = "pryPonceIEFI";
+            this.Text = "pryPonceIEFI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.Label lbllugardenacimiento;
+        private System.Windows.Forms.Label lblapellido;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblingreso;
+        private System.Windows.Forms.ComboBox combingreso;
+        private System.Windows.Forms.Label lblpuntaje;
+        private System.Windows.Forms.ComboBox combporcentaje;
+        private System.Windows.Forms.Button btnregistrar;
+        private System.Windows.Forms.Button btnvolver;
+        private System.Windows.Forms.ComboBox comblugardenacimiento;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbnmasculino;
+        private System.Windows.Forms.RadioButton rbnfemenino;
+        private System.Windows.Forms.Label lbledad;
+        private System.Windows.Forms.TextBox txtedad;
+    }
+}
+
